2d073c25f1298804d22a6931dfe32b6beaf62a5c
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-03-20 20:53:39 +0100

    Do actually converge in template spec

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -3,6 +3,10 @@ require 'chefspec'
 describe 'The recipe skeleton::default' do
   let (:chef_run) { ChefSpec::ChefRunner.new.converge 'skeleton::default' }
 
+  it 'should converge' do
+    chef_run.should be
+  end
+
   it 'should do something' do
     pending 'Add recipe examples here'
   end
