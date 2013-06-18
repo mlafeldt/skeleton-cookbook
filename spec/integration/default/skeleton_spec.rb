647d8bfbd2a4cb984a9a45f569ea8441c0077d1b
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-06-18 11:06:13 +0200

    Make sample integration test more descriptive
    https://speakerdeck.com/atalanta/test-driven-infrastructure-with-chef?slide=46

--- spec/integration/default/skeleton_spec.rb
+++ spec/integration/default/skeleton_spec.rb
@@ -1,7 +1,11 @@
 require File.expand_path('../../spec_helper', __FILE__)
 
 describe 'default node' do
-  describe package('tree') do
-    it { should be_installed }
+  it 'installs sample package' do
+    expect(package 'tree').to be_installed
+  end
+
+  it 'does something' do
+    pending 'Replace this with meaningful tests'
   end
 end
