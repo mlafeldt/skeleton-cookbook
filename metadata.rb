d20ce884be7dd873fd14d7c5b37f32516ac81caf
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-06-24 14:05:26 +0200

    Replace " with ' in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
-name              "skeleton"
-maintainer        "Mathias Lafeldt"
-maintainer_email  "mathias.lafeldt@gmail.com"
-license           "Apache 2.0"
-description       "Installs/configures something"
+name              'skeleton'
+maintainer        'Mathias Lafeldt'
+maintainer_email  'mathias.lafeldt@gmail.com'
+license           'Apache 2.0'
+description       'Installs/configures something'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.0"
-recipe            "skeleton::default", "Installs/configures something"
+version           '1.0.0'
+recipe            'skeleton::default', 'Installs/configures something'
 
-supports "debian"
-supports "ubuntu"
+supports 'debian'
+supports 'ubuntu'
