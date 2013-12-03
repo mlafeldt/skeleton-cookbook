90a4423d229b5b1bf661c9065db4de3ec6e20658
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-12-03 18:07:16 +0100

    Fix spec_helper to work with serverspec 0.13
    See https://github.com/serverspec/serverspec/commit/797b14015d5a881f5fea531e2df44db672fc1e08#commitcomment-4760150

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -5,7 +5,7 @@ include Serverspec::Helper::DetectOS
 
 RSpec.configure do |c|
   c.before :all do
-    c.os = backend(Serverspec::Commands::Base).check_os
+    c.os = backend(SpecInfra::Command::Base).check_os
     c.path = "/sbin:/usr/sbin"
   end
 end
