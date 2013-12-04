458b246cb340cf80c02b9947adf3fdfa6fcdecde
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-12-04 21:49:59 +0100

    Tweak style of metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-name              'skeleton'
-maintainer        'YOUR NAME'
-maintainer_email  'YOUR EMAIL'
-license           'Apache 2.0'
-description       'Installs/configures something'
-version           '0.1.0'
-recipe            'skeleton::default', 'Installs/configures something'
+name             "skeleton"
+maintainer       "YOUR NAME"
+maintainer_email "YOUR EMAIL"
+license          "Apache 2.0"
+description      "Installs/configures something"
+version          "0.1.0"
+recipe           "skeleton::default", "Installs/configures something"
