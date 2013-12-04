9824b0136d742c17699b5c93bd18be076c3110e8
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-12-04 22:07:05 +0100

    ServerSpec -> Serverspec

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-# Write integration tests with ServerSpec - http://serverspec.org/
+# Write integration tests with Serverspec - http://serverspec.org/
 describe "skeleton::default" do
   it "does something" do
     pending "Replace this with meaningful tests"
